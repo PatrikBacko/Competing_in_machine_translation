a3db785cac431966ddbdc04933d519874199a129
Buciak <patrik.backo1@gmail.com>
2023-11-24 22:29:03 +0100

    Add translation script from Czech to English

---
+++ test_TM-cs-en.py
@@ -0,0 +1,14 @@
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-cs-en")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-cs-en")
+
+# Translate Czech to English
+text = "Stojí mi pipik z kristýnky."
+encoded = tokenizer.encode(text, return_tensors="pt")
+generated_tokens = model.generate(encoded, num_beams=5, num_return_sequences=3)
+result = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+for i, r in enumerate(result):
+    print(f"{i+1}. {r}")
